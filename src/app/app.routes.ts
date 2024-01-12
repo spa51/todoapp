33976fe9bb94307eff02b91244eca07c30036c8b
spa51 <santi.15elspa@outlook.com>
2024-01-12 07:21:03 -0500

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { TestComponentComponent } from './test-component/test-component.component';
+import { Page404Component } from './page404/page404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path:'', //TODO: http://localhost:4200/
+    component: TestComponentComponent
+},
+{
+    path:'test-route',
+    component: TestComponentComponent
+},
+{
+    path:'**',
+    redirectTo:'/'
+}
+];
