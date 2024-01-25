3853231da3b653d28157be9d4f34b5fe4cc4e7ec
spa51 <santi.15elspa@outlook.com>
2024-01-25 17:47:43 -0500

    ng template condicional

--- src/app/modules/task/page/task-page.component.ts
+++ src/app/modules/task/page/task-page.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../components/header/header.component";
 import { ImgBrokenDirective } from '@shared/directives/img-broken.directive';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-task-page',
     standalone: true,
     templateUrl: './task-page.component.html',
     styleUrl: './task-page.component.css',
-    imports: [HeaderComponent,ImgBrokenDirective]
+    imports: [HeaderComponent,ImgBrokenDirective,NgFor,NgIf]
 })
 export class TaskPageComponent {
 
 
     constructor(){}
+
+    results:Array<string> =['hola']
 
     ngOnInit(){
 
