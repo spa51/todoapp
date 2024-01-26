0b3762f19abaeb11b3ed02237970663b1ffdd918
spa51 <santi.15elspa@outlook.com>
2024-01-26 14:43:10 -0500

    ng container

--- src/app/modules/task/page/task-page.component.ts
+++ src/app/modules/task/page/task-page.component.ts
@@ -1,21 +1,42 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../components/header/header.component";
 import { ImgBrokenDirective } from '@shared/directives/img-broken.directive';
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
 
 @Component({
     selector: 'app-task-page',
     standalone: true,
     templateUrl: './task-page.component.html',
     styleUrl: './task-page.component.css',
-    imports: [HeaderComponent,ImgBrokenDirective,NgFor,NgIf]
+    imports: [HeaderComponent,ImgBrokenDirective,NgFor,NgIf,NgTemplateOutlet]
 })
 export class TaskPageComponent {
 
 
     constructor(){}
 
-    results:Array<string> =['hola']
+    listUser:Array<{id:string, name:string}> =[
+        {
+            id:'1',
+            name:'Santiago'
+        },
+        {
+            id:'2',
+            name:'pedro'
+        },
+        {
+            id:'3',
+            name:'camilo'
+        },
+        {
+            id:'4',
+            name:'juan'
+        },
+        {
+            id:'5',
+            name:'fernando'
+        },
+    ]
 
     ngOnInit(){
 
